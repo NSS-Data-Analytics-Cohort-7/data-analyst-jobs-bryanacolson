b2803ed13e106fc7a72b47119b3647aa3f971458
bryanacolson <bryancolson@Bryans-MBP.attlocal.net>
2022-08-06 13:43:21 -0500

    5

---
+++ data/test.sql
@@ -0,0 +1,10 @@
+select count (*) from data_analysts_jobs;
+select *
+from data_analysts_jobs
+FETCH first 10 rows only;
+select count (*)
+from data_analysts_jobs
+where location = 'tn'
+select count (*) 
+from data_anaylsts_jobs 
+where location = 'tn', 'ky'
